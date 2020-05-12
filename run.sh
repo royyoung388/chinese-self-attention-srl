41aa160bfe47975eaad0236060193939e2e13492
Roy Young <976611019@qq.com>
2020-05-13 01:22:48 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -16,16 +16,15 @@ python $TAGGERPATH/tagger/bin/trainer.py \
   --output train \
   --vocabulary $DATAPATH/vocab.txt $DATAPATH/label.txt \
   --parameters="save_summary=false,"`
-               `"feature_size=300,predicate_size=100,hidden_size=600,filter_size=800,"`
-               `"batch_size=2048,buffer_size=10240,num_hidden_layers=10,train_steps=500000,"`
+               `"feature_size=300,predicate_size=100,hidden_size=400,filter_size=800,"`
+               `"batch_size=2048,buffer_size=10240,num_hidden_layers=8,train_steps=500000,"`
                `"relu_dropout=0.2,residual_dropout=0.3,attention_dropout=0.2,lstm_dropout=0.2,"`
                `"optimizer=adadelta,initializer=orthogonal,"`
                `"learning_rate_schedule=piecewise_constant_decay,"`
                `"learning_rate=1,warmup_steps=4000,"`
                `"learning_rate_values=[1.0,0.1,0.01],learning_rate_boundaries=[300000,400000],"`
                `"save_checkpoint_steps=2000,early_stopping=10,plot_step=1000,"`
-               `"device_list=$DEVICE,clip_grad_norm=1.0"
-#,embedding=$EMBPATH/embedding.txt,script=validation.sh
+               `"device_list=$DEVICE,clip_grad_norm=1.0,script=validation.sh,embedding=$EMBPATH/embedding.txt"
 
 #python $TAGGERPATH/tagger/bin/trainer.py \
 #  --model deepatt \
