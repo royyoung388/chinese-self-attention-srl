0f58ec4566e3eaab62a779dda3e95a26c813ec3d
Roy Young <976611019@qq.com>
2020-05-14 23:01:08 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -17,12 +17,12 @@ python $TAGGERPATH/tagger/bin/trainer.py \
   --vocabulary $DATAPATH/vocab.txt $DATAPATH/label.txt \
   --parameters="save_summary=false,"`
                `"feature_size=300,predicate_size=100,hidden_size=400,filter_size=800,"`
-               `"batch_size=2048,buffer_size=10240,num_hidden_layers=12,train_steps=500000,"`
-               `"relu_dropout=0.2,residual_dropout=0.3,attention_dropout=0.2,lstm_dropout=0.2,"`
+               `"batch_size=2048,buffer_size=10240,num_hidden_layers=12,train_steps=600000,"`
+               `"relu_dropout=0.15,residual_dropout=0.15,attention_dropout=0.15,lstm_dropout=0.15,"`
                `"optimizer=adadelta,initializer=orthogonal,"`
                `"learning_rate_schedule=piecewise_constant_decay,"`
                `"learning_rate=1,warmup_steps=4000,"`
-               `"learning_rate_values=[1.0,0.1,0.01],learning_rate_boundaries=[300000,400000],"`
+               `"learning_rate_values=[1.0,0.5,0.25,0.01],learning_rate_boundaries=[400000,470000,54000],"`
                `"save_checkpoint_steps=2000,early_stopping=10,plot_step=1000,"`
                `"device_list=$DEVICE,clip_grad_norm=1.0,script=validation.sh,embedding=$EMBPATH/embedding.txt"
 
